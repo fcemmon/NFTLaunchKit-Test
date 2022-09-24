dbb74000d9fa22010b3652e0ced71265462c2eb6
fcemmon <85023510+carlatic-dev@users.noreply.github.com>
2022-09-24 05:43:41 -0700

    remove error code

--- config/constants/index.ts
+++
@@ -1,7 +0,0 @@
-export const NETWORK_CHAIN_ID = 97; // Testnet
-// export const NETWORK_CHAIN_ID = 56; // Mainnet
-export const BASE_BSC_SCAN_URL = BASE_BSC_SCAN_URLS[NETWORK_CHAIN_ID]; 
-export const RPC_URLS = {
-  56: 'https://bsc-dataseed.binance.org',
-  97: 'https://data-seed-prebsc-1-s1.binance.org:8545'
-};
--- utils/getRpcUrl.js
+++
@@ -1,7 +0,0 @@
-import { RPC_URLS, NETWORK_CHAIN_ID } from '../config/constants';
-
-const getRpcUrl = () => RPC_URLS[NETWORK_CHAIN_ID]
-
-export const nodes = [ RPC_URLS[NETWORK_CHAIN_ID] ]
-
-export default getRpcUrl
